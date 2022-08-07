4be07ad121214f629ece616ad50a9d95c96c6d73
atrain42 <maxheimer4211@gmail.com>
2022-08-07 12:07:34 -0400

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Perhaps far exposed age effects. Now distrusts you her delivered
+        applauded affection out sincerity. As tolerably recommend shameless
+        unfeeling he objection consisted. She although cheerful perceive
+        screened throwing met not eat distance. Viewing hastily or written
+        dearest elderly up weather it as. So direction so sweetness or extremity
+        at daughters. Provided put unpacked now but bringing.
+      </p>
+    </section>
+  );
+};
+
+export default About;
